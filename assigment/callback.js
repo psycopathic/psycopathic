a071f5ff3543f7d321ecae18b7bf4842ec072e02
psycopathic <118753341+psycopathic@users.noreply.github.com>
2024-06-19 03:01:50 +0530

    ok

--- assigment/callback.js
+++ assigment/callback.js
@@ -20,18 +20,54 @@
 
 /*using promise */
 
-const fs = require('fs');
+// const fs = require('fs');
 
-function harshReadFile(){
-    return new Promise(function(resolve){
-        fs.readFile("a.txt","utf-8",function(err,data){
-            resolve(data);
+// function harshReadFile(){
+//     return new Promise(function(resolve){
+//         fs.readFile("a.txt","utf-8",function(err,data){
+//             resolve(data);
+//         });
+//     });
+// }
+
+// function onDone(data){
+//     console.log(data);
+// }
+
+// harshReadFile().then(onDone)
+
+
+//syntax
+// var d = new Promise(function(resolve){
+//     setTimeout(() => {
+//         resolve("foo")
+//     }, 1000);
+// })
+
+// function callback(){
+//     console.log(d);
+// }
+
+// d.then(callback)
+//.then get called whenever the async function resolves...
+
+
+//async await => 
+
+    function harshAsyncFunction(){
+        let p = new Promise(function(resolve){
+            setTimeout(function(){
+                resolve("hi there!");
+
+            },1000);
         });
-    });
-}
+        return p;
+    }
 
-function onDone(data){
-    console.log(data);
-}
+    async function main(){
+        let value = await harshAsyncFunction();
+        console.log(value)
+        //.then hat gya 
+    }
 
-harshReadFile().then(onDone) 
+    main();

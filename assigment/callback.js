4aefb5fc79058d7b29deb0a9c4e6d89fca16165a
psycopathic <118753341+psycopathic@users.noreply.github.com>
2024-06-19 02:31:21 +0530

    ok

--- assigment/callback.js
+++ assigment/callback.js
@@ -1,2 +1,37 @@
-//promises....
+// //promises....
 
+// const { log } = require('console');
+// const fs = require('fs');
+
+
+// function harshReadFile(cb){
+//     fs.readFile("a.txt","utf-8",function(err,data){
+//         cb(data);
+//     });
+// }
+
+// function onDone(data){
+//     console.log(data);
+// }
+
+
+
+// harshReadFile(onDone);
+
+/*using promise */
+
+const fs = require('fs');
+
+function harshReadFile(){
+    return new Promise(function(resolve){
+        fs.readFile("a.txt","utf-8",function(err,data){
+            resolve(data);
+        });
+    });
+}
+
+function onDone(data){
+    console.log(data);
+}
+
+harshReadFile().then(onDone) 

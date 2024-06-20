46b26c4e60f1e84fc8560008b704181a35133d1d
psycopathic <118753341+psycopathic@users.noreply.github.com>
2024-06-20 18:02:58 +0530

    ok

--- assigment/async.js
+++ assigment/async.js
@@ -1,14 +1,14 @@
-function sumOfNumbers(n){
-    var sum = 0;
-    for(let i = 1;i<=n;i++){
-        sum+=i;
-    }
-    console.log(sum);
-}
+// function sumOfNumbers(n){
+//     var sum = 0;
+//     for(let i = 1;i<=n;i++){
+//         sum+=i;
+//     }
+//     console.log(sum);
+// }
 
-function sumOfNumber(){
-    sumOfNumbers(50);
-}
+// function sumOfNumber(){
+//     sumOfNumbers(50);
+// }
 
 
 //to make a asynchronous function synchronous we can use a busy waiting method....
@@ -17,18 +17,42 @@ function sumOfNumber(){
 //like
 //busy waiting
 
-function syncSleep(){
-    let a = 1;
-    for(let i = 1;i<100000000;i++){
-        a++;
-    }
-}
-syncSleep();
-setTimeout(sumOfNumber,2000);
-console.log("Harsh shrivastava");
-console.log("Aditi Mishra");
+// function syncSleep(){
+//     let a = 1;
+//     for(let i = 1;i<100000000;i++){
+//         a++;
+//     }
+// }
+// syncSleep();
+// setTimeout(sumOfNumber,2000);
+// console.log("Harsh shrivastava");
+// console.log("Aditi Mishra");
 
 //some common async function are :
 // setTimeout
 // fs.readFile : to read a file from your filesystem
 // fetch :  to fetch some data from an API endpoints
+
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+//     setTimeout(()=>{
+//         console.log("SetTimeout kicked in....." + `${i}`)
+//     },2000);
+// }
+
+let a = 1;
+console.log(a);
+const fs = require('fs');
+
+fs.readFile("a.txt","utf-8",(err,data)=>{
+    console.log("data read from file is ");
+    console.log(data);
+})
+
+let ans = 0;
+for(let i=1;i<100;i++){
+    ans+=i;
+}
+
+console.log(ans);
